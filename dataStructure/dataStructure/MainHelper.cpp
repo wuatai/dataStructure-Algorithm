a3e684ecd4acf0bdd0984ff9970c78c21874689c
wuatai <2394185881@qq.com>
2019-09-16 21:10:06 +0800

    selectSort
    selectSort

--- dataStructure/dataStructure/MainHelper.cpp
+++ dataStructure/dataStructure/MainHelper.cpp
@@ -1,6 +1,8 @@
 #include "MainHelper.h"
 #include "SortingAlgorithm/Sort.h"
 #include <iostream>
+#include <ctime>
+#include <cassert>
 
 void MainHelper::MainHelper::printArray(const int arr[], const int n)
 {
@@ -12,9 +14,40 @@ void MainHelper::MainHelper::printArray(const int arr[], const int n)
 
 void MainHelper::MainHelper::testSelectionSort()
 {
-	int testArr[10] = { 3, 6, 7, 2, 4, 10, 5, 9, 1, 8 };
-	printArray(testArr, 10);
-	Sorting::SortingAlgorithm::selectionSort(testArr, 10);
-	std::cout << "\n";
-	printArray(testArr, 10);
+	int n = 100000;
+	int *testArr = genRandomArr(n, 1, n);
+	testSortTime("selectionSort", Sorting::SortingAlgorithm::selectionSort, testArr, n);
+}
+
+int* MainHelper::MainHelper::genRandomArr(const int n, const int rangL, const int rangR)
+{
+	assert(rangL <= rangR);
+	int *arr = new int[n];
+	std::srand(time(NULL));
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (rangR - rangL + 1) + rangL;
+	}
+	return arr;
+}
+
+void MainHelper::MainHelper::testSortTime(std::string sortName, void (*sort)(int arr[], int n), int arr[], int n)
+{
+	clock_t startTime = clock();
+	sort(arr, n);
+	clock_t endTime = clock();
+	assert(isSorted(arr, n));
+	std::cout << sortName << " : " << double(endTime - startTime) / CLOCKS_PER_SEC << "s" << std::endl;
+}
+
+bool MainHelper::MainHelper::isSorted(const int arr[], int n)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		if (arr[i] > arr[i+1])
+		{
+			return false;
+		}
+	}
+	return true;
 }
--- dataStructure/dataStructure/MainHelper.h
+++ dataStructure/dataStructure/MainHelper.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_HELPER_H
 #define _MAIN_HELPER_H
+#include <string>
 
 namespace MainHelper
 {
@@ -7,9 +8,12 @@ namespace MainHelper
 	{
 	public:
 		static void printArray(const int arr[], const int n);
-
+		static int *genRandomArr(const int n, const int rangL, const int rangR);
+		static void testSortTime(std::string sortName, void (*sort)(int arr[], int n), int arr[], int n);
+		static bool isSorted(const int arr[], int n);
 	public:
 		static void testSelectionSort();
+		
 	};
 }
 
